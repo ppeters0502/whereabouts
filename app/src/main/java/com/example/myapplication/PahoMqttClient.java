799b921077355f494f6720e166f1e948a050c0eb
ppeters0502 <pspeters93@gmail.com>
2019-04-06 20:23:29 -0500

    PahoMqttClient file finished

--- app/src/main/java/com/example/myapplication/PahoMqttClient.java
+++ app/src/main/java/com/example/myapplication/PahoMqttClient.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -10,6 +11,8 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class PahoMqttClient {
     private static final String TAG = "PahoMQTTClient";
@@ -37,6 +40,78 @@ public class PahoMqttClient {
         }
         return mqttAndroidClient;
     }
+    public void disconnect(@NonNull MqttAndroidClient client) throws MqttException{
+        IMqttToken mqttToken = client.disconnect();
+        mqttToken.setActionCallback(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken iMqttToken) {
+                Log.d(TAG, "Successfully disconnected");
+            }
+            @Override
+            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                Log.d(TAG, "Failed to Disconnect: "+throwable.toString());
+            }
+        });
+    }
+    @NonNull
+    private DisconnectedBufferOptions getDisconnectedBufferOptions(){
+        DisconnectedBufferOptions dbo = new DisconnectedBufferOptions();
+        dbo.setBufferEnabled(true);
+        dbo.setBufferSize(100);
+        dbo.setPersistBuffer(false);
+        dbo.setDeleteOldestMessages(false);
+        return dbo;
+    }
 
+    @NonNull
+    private MqttConnectOptions getMqttConnectionOption() {
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setAutomaticReconnect(true);
+        return mqttConnectOptions;
+    }
+
+    public void publishMessage(@NonNull MqttAndroidClient client, @NonNull String msg, int qos, @NonNull String topic )
+    throws MqttException, UnsupportedEncodingException
+    {
+        byte[] encodedPayload = new byte[0];
+        encodedPayload = msg.getBytes("UTF-8");
+        MqttMessage message = new MqttMessage(encodedPayload);
+        message.setId(320);
+        message.setRetained(true);
+        message.setQos(qos);
+        client.publish(topic, message);
+    }
+    public void subscribe(@NonNull MqttAndroidClient client, @NonNull final String topic, int qos)
+    throws MqttException
+    {
+        IMqttToken token = client.subscribe(topic, qos);
+        token.setActionCallback(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken iMqttToken) {
+                Log.d(TAG, "Subscribe Successfully " + topic);
+            }
+
+            @Override
+            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                Log.e(TAG, "Subscribe Failed " + topic);
+            }
+        });
+    }
+
+    public void unSubscribe(@NonNull MqttAndroidClient client, @NonNull final String topic) throws MqttException{
+        IMqttToken token = client.unsubscribe(topic);
+        token.setActionCallback(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken iMqttToken) {
+                Log.d(TAG, "UnSubscribe Successfully " + topic);
+            }
+
+            @Override
+            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                Log.e(TAG, "UnSubscribe Failed " topic);
+            }
+        });
+    }
 
 }
