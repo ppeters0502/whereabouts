e66758857e1d8ef8069e018aeccbafa5221b30c8
ppeters0502 <pspeters93@gmail.com>
2019-04-07 18:43:29 -0500

    MainActivity progress (still need action listeners set up)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
 
 public class MainActivity extends AppCompatActivity {
+    private MqttAndroidClient client;
+    private String TAG = "MainActivity";
+    private PahoMqttClient pahoMqttClient;
+
+    private EditText textMesage, subscribeTopic, unSubscribeTopic;
+    private Button publishMessage, subscribe, unSubscribe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pahoMqttClient = new PahoMqttClient();
+
+        textMesage = (EditText) findViewById(R.id.publishText);
+        publishMessage = (Button) findViewById(R.id.publishButton);
+
+        subscribe = (Button) findViewById(R.id.subscribeButton);
+        unSubscribe = (Button) findViewById(R.id.unSubscribeButton);
+
+        subscribeTopic = (EditText) findViewById(R.id.subscribeText);
+        unSubscribeTopic = (EditText) findViewById(R.id.unSubscribeText);
+        client = pahoMqttClient.getMqttClient(getApplicationContext(), Constants.MQTT_BROKER_URL, Constants.CLIENT_ID);
+        
     }
 }
