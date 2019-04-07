61e85a138e4a6e49552aa9f2d67e5e0a36d1eaa4
Pat Peters <pspeters93@gmail.com>
2019-04-06 21:21:18 -0500

    Finished MqttMessageService, need to finish MainActivity

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class Constants {
+    public static final String MQTT_BROKER_URL = "tcp://iot.eclipse.org:1883";
+    public static final String PUBLISH_TOPIC = "patp/topic";
+    public static final String CLIENT_ID = "patp";
+}
