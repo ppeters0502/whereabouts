c08e8358ce9a1b9c67167ef16152b9cbcbf94202
ppeters0502 <pspeters93@gmail.com>
2019-05-01 12:55:51 -0500

    updated channel settings from tutorial I got this from

--- android/app/src/main/java/com/example/myapplication/services/NotificationUtility.java
+++ android/app/src/main/java/com/example/myapplication/services/NotificationUtility.java
@@ -11,10 +11,8 @@ import android.support.annotation.RequiresApi;
 
 public class NotificationUtility extends ContextWrapper {
     private NotificationManager mManager;
-    public static final String ANDROID_CHANNEL_ID = "com.chikeandroid.tutsplustalerts.ANDROID";
-    public static final String IOS_CHANNEL_ID = "com.chikeandroid.tutsplustalerts.IOS";
-    public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
-    public static final String IOS_CHANNEL_NAME = "IOS CHANNEL";
+    public static final String GEOFENCE_NOTIFICATION_CHANNEL_ID = "com.example.myapplication.services.GeofenceTransition";
+    public static final String GEOFENCE_NOTIFICATION_NAME = "GEOFENCE CHANNEL";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public NotificationUtility(Context base) {
@@ -26,27 +24,19 @@ public class NotificationUtility extends ContextWrapper {
     public void createChannels() {
 
         // create android channel
-        NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
-                ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationChannel geofenceChannel = new NotificationChannel(GEOFENCE_NOTIFICATION_CHANNEL_ID,
+                GEOFENCE_NOTIFICATION_NAME, NotificationManager.IMPORTANCE_HIGH);
         // Sets whether notifications posted to this channel should display notification lights
-        androidChannel.enableLights(true);
+        geofenceChannel.enableLights(true);
         // Sets whether notification posted to this channel should vibrate.
-        androidChannel.enableVibration(true);
+        geofenceChannel.enableVibration(true);
         // Sets the notification light color for notifications posted to this channel
-        androidChannel.setLightColor(Color.GREEN);
+        geofenceChannel.setLightColor(Color.GREEN);
         // Sets whether notifications posted to this channel appear on the lockscreen or not
-        androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        geofenceChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
 
-        getManager().createNotificationChannel(androidChannel);
+        getManager().createNotificationChannel(geofenceChannel);
 
-        // create ios channel
-        NotificationChannel iosChannel = new NotificationChannel(IOS_CHANNEL_ID,
-                IOS_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-        iosChannel.enableLights(true);
-        iosChannel.enableVibration(true);
-        iosChannel.setLightColor(Color.GRAY);
-        iosChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-        getManager().createNotificationChannel(iosChannel);
     }
 
     private NotificationManager getManager() {
@@ -56,20 +46,12 @@ public class NotificationUtility extends ContextWrapper {
         return mManager;
     }
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public Notification.Builder getAndroidChannelNotification(String title, String body) {
-        return new Notification.Builder(getApplicationContext(), ANDROID_CHANNEL_ID)
+    public Notification.Builder getGeofenceChannelNotification(String title, String body) {
+        return new Notification.Builder(getApplicationContext(), GEOFENCE_NOTIFICATION_CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setSmallIcon(android.R.drawable.stat_notify_more)
                 .setAutoCancel(true);
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public Notification.Builder getIosChannelNotification(String title, String body) {
-        return new Notification.Builder(getApplicationContext(), IOS_CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText(body)
-                .setSmallIcon(android.R.drawable.stat_notify_more)
-                .setAutoCancel(true);
-    }
+    
 }
